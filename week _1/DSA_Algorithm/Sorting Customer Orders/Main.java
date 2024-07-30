17276ea054c93bfd4ea19f87f6195df696d0add7
Jaydeep Das <149702055+JAYDEEPDAS33@users.noreply.github.com>
2024-07-31 01:32:05 +0530

    Update Main.java

--- week _1/DSA_Algorithm/Sorting Customer Orders/Main.java
+++ week _1/DSA_Algorithm/Sorting Customer Orders/Main.java
@@ -3,11 +3,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Order[] orders = {
-            new Order("1", "Alice", 150.50),
-            new Order("2", "Bob", 80.75),
-            new Order("3", "Charlie", 200.00),
-            new Order("4", "David", 50.25),
-            new Order("5", "Eve", 100.00)
+            new Order("1", "Ayan", 150.50),
+            new Order("2", "Bikram", 80.75),
+            new Order("3", "Arpan", 200.00),
+            new Order("4", "Rimi", 50.25),
+            new Order("5", "Souvik", 100.00)
         };
         System.out.println("Before Bubble Sort:");
         Arrays.stream(orders).forEach(System.out::println);
@@ -18,11 +18,11 @@ public class Main {
         Arrays.stream(orders).forEach(System.out::println);
 
         orders = new Order[]{
-            new Order("1", "Alice", 150.50),
-            new Order("2", "Bob", 80.75),
-            new Order("3", "Charlie", 200.00),
-            new Order("4", "David", 50.25),
-            new Order("5", "Eve", 100.00)
+            new Order("1", "Ayan", 150.50),
+            new Order("2", "Bikram", 80.75),
+            new Order("3", "Arpan", 200.00),
+            new Order("4", "Rimi", 50.25),
+            new Order("5", "Souvik", 100.00)
         };
         System.out.println("\nBefore Quick Sort:");
         Arrays.stream(orders).forEach(System.out::println);
